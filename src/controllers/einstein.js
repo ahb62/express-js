c3266b37257b325f12ce0e080f92de81c9cbb449
ahb62 <ahb.62@protonmail.com>
2022-08-03 23:29:39 -0400

    3rd commit. PUT method for the API Rest needed.

---
+++ src/controllers/einstein.js
@@ -0,0 +1,52 @@
+let noruego =
+{
+    "ubicacion": "primera casa",
+    "casa": "Amarillo",
+    "mascota": "",
+    "cigarros": "Dunhill",
+    "bebida": ""
+}
+
+let danes = 
+{
+    "ubicacion": "Segunda casa",
+    "casa": "Azul",
+    "mascota": "caballo",
+    "cigarros": "",
+    "bebida": "te"
+}
+
+
+
+let sueco = 
+{
+    "ubicacion": "tercera casa",
+    "casa": "blanca",
+    "mascota": "perro",
+    "cigarros": "Blends",
+    "bebida": "leche"
+}
+
+
+
+
+let aleman =
+{
+    "ubicacion": "cuarta casa",
+    "casa": "verde",
+    "mascota": "gato",
+    "cigarros": "prince",
+    "bebida": "cafe"
+}
+
+
+
+
+let ingles = 
+{
+    "ubicacion": "ultima casa",
+    "casa": "roja",
+    "mascota": "pajaros",
+    "cigarros": "bluemaster",
+    "bebida": "cerveza"
+}
--- src/index.js
+++ src/index.js
@@ -1,39 +1,20 @@
 //const theMorgan = morgan('combined');
+const { urlencoded } = require('express');
 const express = require('express');
-const {Pool} = require('pg');
+const {thePostgresqlPool} = require('./database.js');
+
+    
+// Initialize the backend
 const app = express();
 const PORT = 3000;
-const myDbData = 
-{   
-    user: "postgres",
-    host: "localhost",
-    database: "crud",
-    password: "root",
-    port: "6000"
-}
-const thePostgresqlPool = new Pool(myDbData);
-thePostgresqlPool.connect();
 app.listen(PORT, () => console.log("server on port " + PORT));
 
-app.get('/', (req, res) => 
-{
-    res.json("Hello World!");
-    console.log("Hello World from the console")
-});
+// Middlewares
+app.use(express.json());
+app.use(urlencoded({extended: false}));
+app.use(require('./routes/routes.js')); 
+
+// Connecting the database
+thePostgresqlPool.connect();
 
 
-thePostgresqlPool.query('SELECT * FROM clients')
-
-    .then(response => {
-
-        console.log(response.rows)
-
-        thePostgresqlPool.end()
-
-    })
-
-    .catch(err => {
-
-        thePostgresqlPool.end()
-
-    })
